e591bed596f566d1679f174fadf3ea74fdf7233c
TWcamel <j0930495159@gmail.com>
2022-07-23 04:17:29 +0800

    remove element two point

--- array/27. Remove Element.java
+++ array/27. Remove Element.java
@@ -1,4 +1,9 @@
 class Solution {
+  /*
+   * method1: brute force
+   * Time: O(MN)
+   * Space: O(1)
+   */
   public int removeElement(int[] nums, int val) {
     if (nums.length == 0)
       return 0;
@@ -14,4 +19,35 @@ class Solution {
     }
     return tail;
   }
+  /*
+   * 分析：此解法只用了一根指針，改進方法可以用雙指針
+   */
+}
+
+
+class Solution {
+    /*
+    method2: two pointer
+    runner: 尋找新array的元素
+    walker: 指向 更新符合條件元素
+    Time: O(N)
+    Space: O(1)
+    */
+    public int removeElement(int[] nums, int val) {
+        if (nums.length == 0) return 0;
+        int runner = 0, walker = 0;
+        while(runner < nums.length) {
+            if(nums[runner] != val)  nums[walker++] = nums[runner];
+            runner++;
+        }
+        return walker;
+    }
+    /*
+    3 2 2 3
+    
+    walker 3
+    runner 3
+    
+    val = 2
+    */
 }
