0864bb4b2155478f46f97f8aa45d6f75b837c578
TWcamel <j0930495159@gmail.com>
2022-07-25 00:14:33 +0800

    typo changed

--- linked list/707. Design Linked List.java
+++ linked list/707. Design Linked List.java
@@ -1,17 +1,13 @@
 class ListNode {
   int val;
   ListNode next;
+  ListNode prev;
 
   ListNode() {
   }
 
   ListNode(int val) {
     this.val = val;
-  }
-
-  ListNode(int val, ListNode next) {
-    this.val = val;
-    this.next = next;
   }
 
 }
